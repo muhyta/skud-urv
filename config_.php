4ab737dd780a073b55148a3cb87822d8bc3e9585
muhyta <muhyta@gmail.com>
2013-03-06 15:17:44 +0600

    1.2.0

--- config_.php
+++ config_.php
@@ -1,7 +1,11 @@
 <?php
 $footer="Фирма"; //---copyrite
-$db=mssql_connect("server","user","passwords"); //---подключение к базе
-mssql_select_db("data_base",$db);
+$db=mssql_connect("server","user","passwords");//---подключение к серверу базы данных СКУД-УРВ
+mssql_select_db("data_base",$db);//---база данных СКУД-УРВ
+$db2_srv="server";//---сервер базы данных MPhones
+$db2_usr="user";//---пользователь сервера базы данных MPhones
+$db2_psw="password";//---пароль пользователя сервера базы данных MPhones
+$db2="db_MPhones";//---база данных MPhones
 $ou=iconv("CP1251","UTF-8","Уволенные");
 $dn = "OU=".$ou.",OU=Users,OU=Group,DC=server,DC=ru";
 $domain = "domain_name";
